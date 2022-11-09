8c6f756a4e1f12bcf3db81eee26dd4e0a388d3d1
Jacob <81290016+ciufcia@users.noreply.github.com>
2022-11-09 17:12:49 +0100

    .

--- test/Archetype/archetypeTest.cpp
+++ test/Archetype/archetypeTest.cpp
@@ -5,9 +5,9 @@ int main() {
     archetype1.setComponent(0, true);
     archetype1.setComponent(1, true);
     archetype1.setComponent(2, true);
-    if (!archetype1.isComponentSet(0)) { return 0;}
-    if (archetype1.isComponentSet(8)) { return 0; }
+    if (!archetype1.isComponentSet(0)) { return 1;}
+    if (archetype1.isComponentSet(8)) { return 1; }
     archetype1.setComponent(1, false);
-    if (archetype1.isComponentSet(1)) { return 0; }
-    return 1;
+    if (archetype1.isComponentSet(1)) { return 1; }
+    return 0;
 }
